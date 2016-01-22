ddcb6eb93ca2083401dadd8e5a87d74a2ee64828
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-01-22 18:22:23 -0500

    rotating hellolabel

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -15,6 +15,7 @@ function init():void {
 }
 
 function gameLoop(event:createjs.Event):void {
+    helloLabel.rotation += 5;
     stage.update();
 }
 
@@ -25,7 +26,7 @@ function main() {
     helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
     helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     helloLabel.x = 320;
-    helloLabel.y = 480;
+    helloLabel.y = 240;
     
     stage.addChild(helloLabel);
 }
